f5db349f8cea527883cb880fcc1c0e5a10e838a9
flyingminer <blookho@gmail.com>
2017-05-05 13:16:54 +0300

    routing errors handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::API
                    status: :not_found,
     }
   end
+
+  def catch_404
+    raise ActionController::RoutingError.new(params[:path])
+  end
+
+  rescue_from ActionController::RoutingError do |exception|
+    logger.error "Routing error occurred: #{exception}"
+    # render plain: '404 Not found', status: 404
+    render json: { error: 'No route matches; check routes',
+                   status: :no_route,
+    }
+  end
+
 end
